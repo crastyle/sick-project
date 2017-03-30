0f2b2ed58c2731c96410d68ad017fb7793cd0830
yewenjuan <13197008000@qq.com>
2017-03-31 00:37:07 +0800

    wenjuan'changed

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -8,37 +8,24 @@ export default {
   data() {
     return {
       msg: 'Welcome to Login',
-      sheetVisible: false,
-      sexStr: '请选择性别',
-      actions: [{
-        name: '男',
-        method: () => {
-          this.sexStr = '男'
-        }
-      }, {
-        name: '女',
-        method: () => {
-          this.sexStr = '女'
-        }
-      }],
+      sheetVisible: true,
       validButtonText: '获取验证码',
       buttonStatus: false
     }
   },
   methods: {
-    sex: function(){
-      this.sheetVisible = true
-    },
     getCode: function() {
+
       let second = 60
       let _this = this
+
       if (_this.buttonStatus) {
         return false
       }
       _this.buttonStatus = true
       this.validButtonText = `${second}重新获取`
       let timer = setInterval(()=>{
-        second -- 
+        second --
         _this.validButtonText = `${second}重新获取`
         if (second === 0) {
           _this.buttonStatus = false
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '../components/Hello'
 import Login from '../pages/login/login.vue'
+import Register from '../pages/register/register.vue'
+import PatientCare from '../pages/patientCare/patientCare.vue'
 //@import
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path: '/',name: 'Hello',component: Hello},
-    {path: '/login',name: 'Login',component: Login}//@register
+    {path: '/login',name: 'Login',component: Login},
+    {path: '/register',name: 'Register',component: Register},
+    {path: '/patientCare',name: 'PatientCare',component: PatientCare}//@register
 
   ]
 })
