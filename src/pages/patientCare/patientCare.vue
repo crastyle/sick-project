6bdaad84327216246dc7fc57fa4cf59cd41feb91
yewenjuan <13197008000@qq.com>
2017-04-05 12:45:40 +0800

    wenjuan'add changed patientcare

--- src/pages/patientCare/patientCare.vue
+++ src/pages/patientCare/patientCare.vue
@@ -4,28 +4,19 @@
     <p>注册前请完成以下4个题目</p>
     <div class="content" @click="go">
       <p>2.临床上以血红蛋白值佐为判断贫血的依据。</p>
-      <mt-radio
-        v-model="value"
-        :options="['先快后慢', '先慢后快', '先慢后快']">
-      </mt-radio>
       <div class="page-swipe">
-        <mt-swipe :auto="4000">
+        <mt-swipe :auto="0">
           <mt-swipe-item>
-
+            <mt-radio v-model="value" :options="['先快后慢', '先慢后快', '先慢后快', '匀速上升']"></mt-radio>
           </mt-swipe-item>
           <mt-swipe-item>
-            <mt-radio
-              title="radio list"
-              v-model="value"
-              :options="['optionA', 'optionB', 'optionC']">
-            </mt-radio>
+            <mt-radio v-model="value" :options="['先快后慢', '先慢后快', '先慢后快', '匀速上升']"></mt-radio>
           </mt-swipe-item>
           <mt-swipe-item>
-            <mt-radio
-              title="radio list"
-              v-model="value"
-              :options="['optionA', 'optionB', 'optionC']">
-            </mt-radio>
+            <mt-radio v-model="value" :options="['先快后慢', '先慢后快', '先慢后快', '匀速上升']"></mt-radio>
+          </mt-swipe-item>
+          <mt-swipe-item>
+            <mt-radio v-model="value" :options="['先快后慢', '先慢后快', '先慢后快', '匀速上升']"></mt-radio>
           </mt-swipe-item>
         </mt-swipe>
       </div>
